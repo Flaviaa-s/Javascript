bddbafeb4be6e10a21d9762b5849dc7667cb5adc
Flávia Silva <flaviasf08@gmail.com>
2024-04-20 12:06:15 -0300

    Aula 3 - If else e for

---
+++ Aula03/js/script3.js
@@ -0,0 +1,17 @@
+function verifica(){
+
+    var nota =  document.getElementsByTagName("input")[0].value;
+    var resultado="aprovado";
+    if(nota >= 7){
+        resultado;
+    }
+    
+        else if(nota <= 4){
+            resultado = "reprovado"
+        }
+
+        else{
+            resultado ="recuperação";
+        }
+        document.getElementsByTagName("label")[1].innerHTML=resultado;
+}
