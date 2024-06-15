9985268a226d9fc90c574836fe5968d4f4b0f318
Flávia Silva <flaviasf08@gmail.com>
2024-06-15 12:06:37 -0300

    Aula 9, criação de animações utilizando jquery

---
+++ Aula09/jquery/js/script.js
@@ -0,0 +1,6 @@
+    $(function(){
+        $("#mover").draggable()
+        $("#mover").resizable()
+        $("#categorias").accordion()
+    })
+
---
+++ Aula09/js/script.js
@@ -0,0 +1,8 @@
+// Referenciar a div camamda
+const camada = document.querySelector("#camada");
+setTimeout(()=>{
+    camada.style.opacity="0" 
+    //camada.style.top="-1000px"
+    camada.style.height="0vh"
+    camada.style.width="0vw"
+},4000)
